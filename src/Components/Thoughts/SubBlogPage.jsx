bf2a7e808c31faa87846a22b829a09fa2aacc8a1
Nirmala8645 <nr971031@gmail.com>
2026-03-16 10:17:20 +0530

    Update SubBlogPage.jsx

--- src/Components/Thoughts/SubBlogPage.jsx
+++ src/Components/Thoughts/SubBlogPage.jsx
@@ -331,7 +331,7 @@ export const SubBlogPage = ({ blogId, onBack }) => {
         </div>
       </div>
 
-      <CTASection backgroundColor="#FFF1F1"
+      <CTASection backgroundColor="#FFF1F1" className="mt-[50px]"
         heading={blog.cta?.heading || "Ready to Elevate Your Packaging?"}
         subheading={
           blog.cta?.subheading ||
