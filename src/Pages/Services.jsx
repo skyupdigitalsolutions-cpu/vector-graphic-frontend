b984a51d5dbed09d4205ededb11973d80c0c76ee
SKYUP DIGITAL SOLUTIONS <skyupdigitalsolutions@gmail.com>
2026-04-08 12:45:27 +0530

    Update Services.jsx

--- src/Pages/Services.jsx
+++ src/Pages/Services.jsx
@@ -182,7 +182,7 @@ const Services = () => {
       <Packaging />
       <ClientMarquee />
       <FAQSection faqs={enquire} />
-        <CTASection heading="Get your project plan today." subheading="Turn your ideas into reality."/>
+        <CTASection heading="Get plan today." subheading="Turn your ideas into reality."/>
       <Footer />
     </>
   );
